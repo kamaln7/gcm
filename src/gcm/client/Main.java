6ff1a47cedb78c24a68faf92d4b7981cbfb989cb
Kamal Nasser <hello@kamal.io>
2019-04-24 13:10:33 +0300

    init

---
+++ src/gcm/client/Main.java
@@ -0,0 +1,7 @@
+package gcm.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is the client");
+    }
+}
---
+++ src/gcm/server/Main.java
@@ -0,0 +1,7 @@
+package gcm.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is the server");
+    }
+}
