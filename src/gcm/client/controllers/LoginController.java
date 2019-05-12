0942a44f78c1a4ce9807d7661dc1a203834446fa
Kamal Nasser <hello@kamal.io>
2019-05-12 20:09:55 +0300

    Limit username to 255 characters

--- src/gcm/client/controllers/LoginController.java
+++ src/gcm/client/controllers/LoginController.java
@@ -8,20 +8,19 @@ import gcm.database.models.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
-    @FXML
-    private Hyperlink openRegisterViewBtn;
-
+public class LoginController implements Initializable {
     @FXML
     private TextField usernameField;
 
@@ -67,6 +66,23 @@ public class LoginController {
             RegisterController.loadView(ClientGUI.getPrimaryStage());
         } catch (Exception e) {
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        usernameField.setTextFormatter(new TextFormatter<String>(change -> {
+            if (!change.isAdded()) {
+                return change;
+            }
+
+            // if the text will be more longer than 255, exit
+            if (change.getControlNewText().length() > 255) {
+                return null;
+            }
+
+            // otherwise go on
+            return change;
+        }));
     }
 }
 
--- src/gcm/client/controllers/RegisterController.java
+++ src/gcm/client/controllers/RegisterController.java
@@ -8,17 +8,20 @@ import gcm.database.models.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
-public class RegisterController {
+public class RegisterController implements Initializable {
     @FXML
     private TextField usernameField;
 
@@ -72,6 +75,23 @@ public class RegisterController {
             LoginController.loadView(ClientGUI.getPrimaryStage());
         } catch (Exception e) {
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        usernameField.setTextFormatter(new TextFormatter<String>(change -> {
+            if (!change.isAdded()) {
+                return change;
+            }
+
+            // if the text will be more longer than 255, exit
+            if (change.getControlNewText().length() > 255) {
+                return null;
+            }
+
+            // otherwise go on
+            return change;
+        }));
     }
 }
 
