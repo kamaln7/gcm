efceaab77d861b507825f671b2856ee5f98ec9f1
saedtoure <saedaltory@gmail.com>
2019-05-31 20:19:01 +0300

    added img but not finished

--- src/gcm/client/controllers/AddAttractionController.java
+++ src/gcm/client/controllers/AddAttractionController.java
@@ -1,10 +1,7 @@
 package gcm.client.controllers;
 
 import gcm.client.bin.ClientGUI;
-import gcm.commands.FindMapByTitleAndVersionCommand;
-import gcm.commands.FindMapByTitleCommand;
-import gcm.commands.Input;
-import gcm.commands.Response;
+import gcm.commands.*;
 import gcm.database.models.Map;
 import gcm.database.models.User;
 import javafx.embed.swing.SwingFXUtils;
@@ -13,21 +10,34 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import javafx.scene.image.ImageView;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.text.AttributedCharacterIterator;
 
 public class AddAttractionController {
+
+    @FXML
+    private AnchorPane pane;
 
     @FXML
     private ImageView mapImg;
@@ -37,6 +47,12 @@ public class AddAttractionController {
 
     @FXML
     private TextField version_field;
+
+    @FXML
+    private TextField Xcord;
+
+    @FXML
+    private TextField Ycord;
 
     public static void loadView(Stage primaryStage) throws IOException {
         URL url = MainScreenController.class.getResource("/gcm/client/views/AddAttraction.fxml");
@@ -49,32 +65,34 @@ public class AddAttractionController {
         primaryStage.show();
     }
 
-
-
     @FXML
     void showMap(ActionEvent event) {
-        Input input = new FindMapByTitleCommand.Input(title_field.getText());
+        Input input = new FindMapByTitleAndVersionCommand.Input(title_field.getText(), version_field.getText());
 
         try {
             Response response = ClientGUI.getClient().sendInputAndWaitForResponse(input);
             FindMapByTitleAndVersionCommand.Output output = response.getOutput(FindMapByTitleAndVersionCommand.Output.class);
-            System.out.println("map title:"+ output.map.getVersion());
-            System.out.println(output.imgBytes);
-//            System.out.println(output.getImageBytes().length);
-//            for(int i=0; i< output.getImageBytes().length; i++) {
-//                System.out.print(output.getImageBytes()[i] +" ");
-//            }
 
-//            ByteArrayInputStream bis = new ByteArrayInputStream(output.img);
-//            BufferedImage bImage2 = ImageIO.read(bis);
-//            Image image = SwingFXUtils.toFXImage(bImage2, null);
-//            mapImg.setImage(image);
+            BufferedImage bImage = ImageIO.read(new ByteArrayInputStream(output.imgBytes));
+            Image image = SwingFXUtils.toFXImage(bImage, null);
+            mapImg.setImage(image);
+            mapImg.setOnMouseClicked(e -> {
+                System.out.println("["+e.getX()+", "+e.getY()+"]");
+                Xcord.setText(String.valueOf(e.getX()));
+                Ycord.setText(String.valueOf(e.getY()));
+                Circle c = new Circle(e.getX(), e.getY(), 5, Color.RED);
+                pane.getChildren().add(c);
+                mapImg.setOnMouseClicked(null);
+            });
+            BufferedImage bImage2 = SwingFXUtils.fromFXImage(mapImg.getImage(), null);
+            ByteArrayOutputStream s = new ByteArrayOutputStream();
+            ImageIO.write(bImage, "jpg", s);
+            ImageIO.write(bImage2, "jpg", new File("outputtest.jpg") );
+            byte[] res  = s.toByteArray();
+            s.close();
 
-
-
-
-        } catch (User.NotFound e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Incorrect login details.");
+        } catch (Map.NotFound e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Map Not Found");
             alert.show();
         } catch (Exception e) {
             ClientGUI.showErrorTryAgain();
