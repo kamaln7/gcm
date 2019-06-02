9d496df6ab5524213cb446524d3c2c8dce7f6cee
Kamal Nasser <hello@kamal.io>
2019-06-02 16:51:52 +0300

    split search into its own controller & page

---
+++ src/gcm/client/controllers/SearchCityOrAttractionController.java
@@ -0,0 +1,16 @@
+package gcm.client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SearchCityOrAttractionController {
+    @FXML
+    private TextField searchQueryTF;
+
+
+    @FXML
+    void searchButtonClick(ActionEvent event) {
+
+    }
+}
