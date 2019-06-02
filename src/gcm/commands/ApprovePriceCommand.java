a415128095f5167019707688b0307a774056835a
Amin <aminshbi@gmail.com>
2019-06-02 21:55:26 +0300

    Approve Price Changing -- Not finished

--- src/gcm/commands/ApprovePriceCommand.java
+++ src/gcm/commands/ApprovePriceCommand.java
@@ -4,31 +4,30 @@ import gcm.database.models.City;
 import gcm.server.Server;
 import ocsf.server.ConnectionToClient;
 
+import java.sql.Array;
 import java.sql.ResultSet;
 
 public class ApprovePriceCommand implements Command {
     public static class Input extends gcm.commands.Input {
 
 
-        public Input() {
 
-        }
     }
 
     public static class Output extends gcm.commands.Output {
-        public ResultSet rs;
+        public String[] result;
 
 
-        public Output(ResultSet rs) {
-            this.rs= rs;
+        public Output(String[] result) {
+            this.result= result;
         }
     }
 
     @Override
     public Output runOnServer(Request request, Server server, ConnectionToClient client) throws Exception {
         Input input = request.getInput(Input.class);
-        ResultSet rs = City.findUnapproved();
+        String[] result = City.findUnapproved();
 
-        return new Output(rs);
+        return new Output(result);
     }
 }
