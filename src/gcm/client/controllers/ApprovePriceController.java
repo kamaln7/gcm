eefe596b7dd8ef044005bd8681388d9f6773fb14
Amin <aminshbi@gmail.com>
2019-06-02 18:23:34 +0300

    Approve Price Changing

--- src/gcm/client/controllers/ApprovePriceController.java
+++ src/gcm/client/controllers/ApprovePriceController.java
@@ -1,16 +1,17 @@
 package gcm.client.controllers;
 
 import gcm.client.bin.ClientGUI;
-import gcm.commands.ChangePriceCommand;
-import gcm.commands.FindCityCommand;
-import gcm.commands.Input;
-import gcm.commands.Response;
+import gcm.commands.*;
 import gcm.database.models.City;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -18,9 +19,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
-public class AprovePriceController {
+public class ApprovePriceController {
     public static void loadView(Stage primaryStage) throws IOException {
-        URL url = MainScreenController.class.getResource("/gcm/client/views/ChangePrice.fxml");
+        URL url = MainScreenController.class.getResource("/gcm/client/views/ApprovePrice.fxml");
         AnchorPane pane = FXMLLoader.load(url);
         Scene scene = new Scene(pane);
         // setting the stage
@@ -31,18 +32,24 @@ public class AprovePriceController {
     }
     @FXML
     void getPrice(ActionEvent event) {
-        String cityName = cityField.getText();
+        /*String cityName = cityField.getText();
         String countyName = countryField.getText();
-
-        Input input = new FindCityCommand.Input(cityName, countyName);
+*/
+        Input input = new ApprovePriceCommand.Input();
 
         try {
             Response response = ClientGUI.getClient().sendInputAndWaitForResponse(input);
-            FindCityCommand.Output output = response.getOutput(FindCityCommand.Output.class);
-            Double price1 = output.city.getPurchase_price();
-            Double price2 = output.city.getSubscription_price();
-            PurchasePriceField.setText(String.format("%.2f",price1));
-            SubPriceField.setText(String.format("%.2f",price2));
+            ApprovePriceCommand.Output output = response.getOutput(ApprovePriceCommand.Output.class);
+
+            while(output.rs.next()){
+                oblist.add(new City(output.rs.getString("name"),
+                        output.rs.getString("country"), output.rs.getDouble("purchase_price"),
+                        output.rs.getDouble("subscription_price"),
+                        output.rs.getDouble("new_purchase_price"),
+                        output.rs.getDouble("new_sub_price")));
+
+            }
+
 
         } catch (City.NotFound e) {
             Alert alert = new Alert(Alert.AlertType.ERROR, "City is not found");
@@ -52,8 +59,11 @@ public class AprovePriceController {
             e.printStackTrace();
         }
 
+        tableList.setItems(oblist);
+
 
     }
+    /*
     @FXML
     void UpdatePrice(ActionEvent event) {
         String cityName = cityField.getText();
@@ -75,9 +85,29 @@ public class AprovePriceController {
             ClientGUI.showErrorTryAgain();
             e.printStackTrace();
         }
-    }
+    }*/
     @FXML
-    private TableView<cities> tableList;
+    private TableView tableList;
+
+    private ObservableList<City> oblist = FXCollections.observableArrayList();
+
+    @FXML
+    private TableColumn<City, String> city;
+
+    @FXML
+    private TableColumn<City, String> country;
+
+    @FXML
+    private TableColumn<City, Double> old_purchase;
+
+    @FXML
+    private TableColumn<City, Double> new_purchase;
+
+    @FXML
+    private TableColumn<City, Double> old_sub;
+
+    @FXML
+    private TableColumn<City, Double> new_sub;
 
     @FXML
     void ApprovePrice(ActionEvent event) {
