1eaa1257fa8f9b1b0a5fbe963405cf306d17e32f
Kamal Nasser <hello@kamal.io>
2019-06-02 14:09:47 +0300

    Set directory to store server files in

--- src/gcm/commands/AddAttractionAndUpdateMapImageCommand.java
+++ src/gcm/commands/AddAttractionAndUpdateMapImageCommand.java
@@ -38,23 +38,23 @@ public class AddAttractionAndUpdateMapImageCommand implements Command {
     }
 
     @Override
-    public Output runOnServer(Request request, Server server, ConnectionToClient client) throws Exception{
+    public Output runOnServer(Request request, Server server, ConnectionToClient client) throws Exception {
         Input input = request.getInput(Input.class);
         //store the img in the server
         String imageName = UUID.randomUUID().toString();
-        Files.write(Paths.get(imageName), input.new_map_image);
+        Files.write(Paths.get(server.getFilesPath(), imageName), input.new_map_image);
 
-        Map mapToUpdate = Map.findByTitleAndVersion(input.map_title,input.map_version);
+        Map mapToUpdate = Map.findByTitleAndVersion(input.map_title, input.map_version);
         //delete the old file from the server
-        File file = new File(mapToUpdate.getImg());
+        File file = new File(server.getFilesPath(), mapToUpdate.getImg());
         file.delete();
 
 
-        mapToUpdate.updateImage(imageName,input.map_version,input.map_title);
+        mapToUpdate.updateImage(imageName, input.map_version, input.map_title);
 
         City city = City.findById(mapToUpdate.getCityId());
 
-        Attraction attraction = new Attraction(input.attraction_name,city.getName(),input.attraction_type,input.getAttraction_location);
+        Attraction attraction = new Attraction(input.attraction_name, city.getName(), input.attraction_type, input.getAttraction_location);
         attraction.insert();
 
         return new Output();
