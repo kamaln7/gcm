733ffa25f8d99a6c47589dcfc934075f3ef1572b
Kamal Nasser <hello@kamal.io>
2019-06-03 16:50:47 +0300

    use Tahoma as the font everywhere

--- src/gcm/client/controllers/MainScreenController.java
+++ src/gcm/client/controllers/MainScreenController.java
@@ -13,7 +13,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
@@ -210,7 +209,7 @@ public class MainScreenController implements Initializable {
             }
         }
     }
-  
+
     @FXML
     void changePrice(ActionEvent event) {
         try {
