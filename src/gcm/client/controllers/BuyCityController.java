9c562caa12fae35d108d39cea432aa7f7235ffca
Waseem Semaan <waseem96@outlook.com>
2019-06-07 20:17:11 +0300

    added renew subscription

--- src/gcm/client/controllers/BuyCityController.java
+++ src/gcm/client/controllers/BuyCityController.java
@@ -3,6 +3,7 @@ package gcm.client.controllers;
 import gcm.client.bin.ClientGUI;
 import gcm.commands.*;
 import gcm.database.models.City;
+import gcm.database.models.Subscription;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -71,22 +72,57 @@ public class BuyCityController {
 
         int city_id = city.getId();
         int user_id = ClientGUI.getCurrentUser().getId();
+        double price = city.getSubscriptionPrice();
+
+        Date temp;
+
 
         Date from_date = new Date();
         Calendar to_date = Calendar.getInstance();
         to_date.setTime(from_date);
         to_date.add(Calendar.MONTH, 6);
 
-        Input input = new AddSubscriptionToDataBaseCommand.Input(user_id, city_id, from_date, to_date.getTime());
 
+
+        Input input1 = new FindSubscriptionCommand.Input(user_id, city_id,from_date);
         try {
-            Response response = ClientGUI.getClient().sendInputAndWaitForResponse(input);
-            AddCityToDataBaseCommand.Output output = response.getOutput(AddCityToDataBaseCommand.Output.class);
+            Response response = ClientGUI.getClient().sendInputAndWaitForResponse(input1);
+            FindSubscriptionCommand.Output output = response.getOutput(FindSubscriptionCommand.Output.class);
+           // new Alert(Alert.AlertType.WARNING, "Subscriptio already exists and ends at + " + output.subscription.getToDate()).show();
+            try {
+                RenewSubscriptionController.loadView(ClientGUI.getPrimaryStage(), output.subscription, city.getSubscriptionPrice());
+            }
+            catch (Exception e) {
+                ClientGUI.showErrorTryAgain();
+                e.printStackTrace();
+            }
+        }
+        catch (Subscription.NotFound e){ // if not found then insert a new subscription
+            Input input = new AddSubscriptionToDataBaseCommand.Input(user_id, city_id, from_date, to_date.getTime(), price);
+            try {
+                Response response = ClientGUI.getClient().sendInputAndWaitForResponse(input);
+                AddSubscriptionToDataBaseCommand.Output output = response.getOutput(AddSubscriptionToDataBaseCommand.Output.class);
+                new Alert(Alert.AlertType.INFORMATION, " your subscription end at: " + to_date.getTime()).show();
+                try {
+                    MainScreenController.loadView(ClientGUI.getPrimaryStage());
+                }catch (Exception e1){}
+            }
+            catch (Exception e1) {
+                ClientGUI.showErrorTryAgain();
+                e.printStackTrace();
+            }
+        }
 
-        }  catch (Exception e) {
+        catch (Exception e) {
             ClientGUI.showErrorTryAgain();
             e.printStackTrace();
         }
+
+
+
+
+
+
 
     }
 
@@ -96,8 +132,9 @@ public class BuyCityController {
 
         int city_id = city.getId();
         int user_id = ClientGUI.getCurrentUser().getId();
+        double price = city.getPurchasePrice();
 
-        Input input = new AddPurchaseToDataBaseCommand.Input(user_id,city_id);
+        Input input = new AddPurchaseToDataBaseCommand.Input(user_id,city_id,price);
 
         try {
             Response response = ClientGUI.getClient().sendInputAndWaitForResponse(input);
