ca5a81d53f7fdbe8584b7a941cb2d1fc438f40c9
Kamal Nasser <hello@kamal.io>
2019-06-11 15:07:27 +0300

    job scheduler

---
+++ src/gcm/server/jobs/SubscriptionExpiryNotification.java
@@ -0,0 +1,33 @@
+package gcm.server.jobs;
+
+import gcm.database.models.ServerJob;
+import gcm.server.Server;
+
+public class SubscriptionExpiryNotification extends Job {
+    @Override
+    public long getInterval() {
+        return 10;
+    }
+
+    @Override
+    public String getName() {
+        return "subscription_expiry";
+    }
+
+    public SubscriptionExpiryNotification(Server server) {
+        super(server);
+    }
+
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Void call() throws Exception {
+        System.out.println("Sub job run");
+        (new ServerJob(getName())).insert();
+        return null;
+    }
+}
